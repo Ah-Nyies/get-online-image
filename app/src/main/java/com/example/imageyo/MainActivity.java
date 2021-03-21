71403f3580e6e36c1706e93685bad3140307f820
Ah Nyies <nyinyismg600@gmail.com>
2021-03-21 20:37:11 +0630

    New Online Image Loader

---
+++ app/src/main/java/com/example/imageyo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.imageyo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
